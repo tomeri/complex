7bad883b95d49b0f2b4fefaf47b71ccbe951d9c1
Tomas Kelemen <>
2020-04-20 22:47:41 +0200

    Fix seenIndexes update if fetch unsuccessful

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -22,7 +22,9 @@ class Fib extends Component {
     async fetchIndexes() {
         const seenIndexes = await axios.get('/api/values/all')
 
-        this.setState({ seenIndexes: seenIndexes.data })
+        if (seenIndexes) {
+            this.setState({ seenIndexes: seenIndexes.data })
+        }
     }
 
     renderSeenIndexes() {
